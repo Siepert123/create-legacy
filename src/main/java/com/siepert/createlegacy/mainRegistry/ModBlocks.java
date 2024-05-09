e99b041fb361fc9b9ba2cea2e0167fa031042758
Siepert123 <sj.hijmans@gmail.com>
2024-05-09 22:50:49 +0200

    c418 is plaeing

--- src/main/java/com/siepert/createlegacy/mainRegistry/ModBlocks.java
+++ src/main/java/com/siepert/createlegacy/mainRegistry/ModBlocks.java
@@ -1,7 +1,10 @@
 package com.siepert.createlegacy.mainRegistry;
 
 import com.siepert.createlegacy.blocks.*;
+import com.siepert.createlegacy.blocks.logic.BlockScheduleCook;
+import com.siepert.createlegacy.blocks.logic.BlockScheduleWash;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,4 +16,7 @@ public class ModBlocks {
     public static final Block MATERIAL_STORAGE_BLOCK = new BlockMaterialStorage();
     public static final Block CASING_BLOCK = new BlockCasing();
     public static final Block COGWHEEL = new BlockCogwheel("cogwheel");
+
+    public static final Block SCHEDULE_WASH = new BlockScheduleWash();
+    public static final Block SCHEDULE_COOK = new BlockScheduleCook();
 }
