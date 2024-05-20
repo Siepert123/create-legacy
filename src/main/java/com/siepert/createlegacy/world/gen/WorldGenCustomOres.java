a2f91713e0fd1456d980583c1cde21d474130fe4
Siepert123 <sj.hijmans@gmail.com>
2024-05-20 20:32:43 +0200

    new stones should gen

--- src/main/java/com/siepert/createlegacy/world/gen/WorldGenCustomOres.java
+++ src/main/java/com/siepert/createlegacy/world/gen/WorldGenCustomOres.java
@@ -21,15 +21,39 @@ public class WorldGenCustomOres implements IWorldGenerator {
     private WorldGenerator ore_zinc;
     private WorldGenerator stone_calcite;
     private WorldGenerator stone_tuff;
+
+    private WorldGenerator stone_asurine;
+    private WorldGenerator stone_crimsite;
+    private WorldGenerator stone_limestone;
+    private WorldGenerator stone_ochrum;
+    private WorldGenerator stone_scorchia;
+    private WorldGenerator stone_scoria;
+    private WorldGenerator stone_veridium;
     public WorldGenCustomOres() {
         ore_copper = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.COPPER),
                 10, BlockMatcher.forBlock(Blocks.STONE));
         ore_zinc = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.ZINC),
                 6, BlockMatcher.forBlock(Blocks.STONE));
+
         stone_calcite = new WorldGenMinable(ModBlocks.MODERN_STONE.getDefaultState().withProperty(BlockModernStone.VARIANT, EnumHandler.ModernStoneEnumType.CALCITE),
                 40, BlockMatcher.forBlock(Blocks.STONE));
         stone_tuff = new WorldGenMinable(ModBlocks.MODERN_STONE.getDefaultState().withProperty(BlockModernStone.VARIANT, EnumHandler.ModernStoneEnumType.CALCITE),
                 40, BlockMatcher.forBlock(Blocks.STONE));
+
+        stone_asurine = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.ASURINE),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_crimsite = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.CRIMSITE),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_limestone = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.LIMESTONE),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_ochrum = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.OCHRUM),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_scorchia = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.SCORCHIA),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_scoria = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.SCORIA),
+                30, BlockMatcher.forBlock(Blocks.STONE));
+        stone_veridium = new WorldGenMinable(ModBlocks.ORE.getDefaultState().withProperty(BlockOre.VARIANT, EnumHandler.OreEnumType.VERIDIUM),
+                30, BlockMatcher.forBlock(Blocks.STONE));
     }
 
     @Override
@@ -37,14 +61,24 @@ public class WorldGenCustomOres implements IWorldGenerator {
                          IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         switch (world.provider.getDimension()) {
             case -1:
+                runGenerator(stone_scoria, world, random, chunkX, chunkZ, 3, 10, 70);
+                runGenerator(stone_scorchia, world, random, chunkX, chunkZ, 3, 60, 110);
                 break;
 
             case 0:
                 runGenerator(ore_copper, world, random, chunkX, chunkZ, 8, 25, 128);
                 runGenerator(ore_zinc, world, random, chunkX, chunkZ, 7, 5, 40);
+
                 runGenerator(stone_calcite, world, random, chunkX, chunkZ, 4, 20, 40);
                 runGenerator(stone_tuff, world, random, chunkX, chunkZ, 4, 0, 20);
+
+                runGenerator(stone_asurine, world, random, chunkX, chunkZ, 1, 10, 40);
+                runGenerator(stone_crimsite, world, random, chunkX, chunkZ, 1, 40, 64);
+                runGenerator(stone_limestone, world, random, chunkX, chunkZ, 1, 30, 50);
+                runGenerator(stone_ochrum, world, random, chunkX, chunkZ, 1, 10, 40);
+                runGenerator(stone_veridium, world, random, chunkX, chunkZ, 1, 40, 64);
                 break;
+
             case 1:
                 break;
         }
