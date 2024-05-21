b8a16439c6024685e51d146b9d69b049c6c74659
Siepert123 <sj.hijmans@gmail.com>
2024-05-21 07:07:40 +0200

    more comments

--- src/main/java/com/siepert/createlegacy/items/ItemVariants.java
+++ src/main/java/com/siepert/createlegacy/items/ItemVariants.java
@@ -2,6 +2,7 @@ package com.siepert.createlegacy.items;
 
 import net.minecraft.item.Item;
 
+/**Useful class for having blocks with subtypes having items. */
 public class ItemVariants extends Item {
     public ItemVariants() {
 
--- src/main/java/com/siepert/createlegacy/util/Reference.java
+++ src/main/java/com/siepert/createlegacy/util/Reference.java
@@ -1,11 +1,10 @@
 package com.siepert.createlegacy.util;
 
-import org.apache.logging.log4j.Logger;
-
+/**Some basic variables. */
 public class Reference {
-    public static final String MOD_ID = "create";
-    public static final String NAME = "Create Legacy";
-    public static final String VERSION = "1.0";
-    public static final String CLIENT = "com.siepert.createlegacy.proxy.ClientProxy";
-    public static final String COMMON = "com.siepert.createlegacy.proxy.CommonProxy";
+    public static final String MOD_ID = "create"; //Mod ID; will never change
+    public static final String NAME = "Create Legacy"; //Mod name; will never change
+    public static final String VERSION = "0.1.0"; //The version, changes each update in this fashion: rework.major.minor.patch
+    public static final String CLIENT = "com.siepert.createlegacy.proxy.ClientProxy"; //Client proxy class
+    public static final String COMMON = "com.siepert.createlegacy.proxy.CommonProxy"; //Common proxy class
 }
