2ca12e80d4e5a96fd64a9c5883e530bdb8ad12ca
Siepert123 <sj.hijmans@gmail.com>
2024-05-21 07:12:36 +0200

    h

--- src/main/java/com/siepert/createlegacy/items/ItemVariants.java
+++ src/main/java/com/siepert/createlegacy/items/ItemVariants.java
@@ -1,7 +1,8 @@
 package com.siepert.createlegacy.items;
 
 import net.minecraft.item.Item;
-
+/**...What does this even do?? */
+@Deprecated
 public class ItemVariants extends Item {
     public ItemVariants() {
 
