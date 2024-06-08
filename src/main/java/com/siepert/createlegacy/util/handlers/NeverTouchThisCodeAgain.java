172ffc9bbcd288dfb6a09e7e726e74f6c712f2bc
Siepert123 <sj.hijmans@gmail.com>
2024-06-08 18:38:22 +0200

    @NeverTouchThisCodeAgain

---
+++ src/main/java/com/siepert/createlegacy/util/handlers/NeverTouchThisCodeAgain.java
@@ -0,0 +1,8 @@
+package com.siepert.createlegacy.util.handlers;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface NeverTouchThisCodeAgain {
+}
