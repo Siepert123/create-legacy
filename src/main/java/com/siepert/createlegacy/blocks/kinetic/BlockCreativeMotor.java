8ab448d64e7d5e08fb0fa6b9b31f7174b86af7f9
Siepert123 <sj.hijmans@gmail.com>
2024-06-08 23:04:54 +0200

    Started working on creative motor

---
+++ src/main/java/com/siepert/createlegacy/blocks/kinetic/BlockCreativeMotor.java
@@ -0,0 +1,107 @@
+package com.siepert.createlegacy.blocks.kinetic;
+
+import com.siepert.createlegacy.CreateLegacy;
+import com.siepert.createlegacy.mainRegistry.ModBlocks;
+import com.siepert.createlegacy.mainRegistry.ModItems;
+import com.siepert.createlegacy.util.IHasModel;
+import com.siepert.createlegacy.util.IKineticActor;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@SuppressWarnings("deprecation")
+public class BlockCreativeMotor extends Block implements IHasModel {
+    public static final PropertyEnum<EnumFacing> FACING = PropertyEnum.<EnumFacing>create("facing", EnumFacing.class);
+    public BlockCreativeMotor(String name) {
+        super(Material.ROCK);
+        this.translucent = true;
+        this.blockSoundType = SoundType.WOOD;
+        this.fullBlock = false;
+        setLightOpacity(0);
+
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        setCreativeTab(CreateLegacy.TAB_CREATE);
+        setHarvestLevel("axe", 0);
+        setHardness(1);
+        setResistance(2);
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+
+
+    }
+
+    @Override
+    public void registerModels() {
+        CreateLegacy.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
+    }
+
+    @Override
+    public boolean isFullBlock(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isTranslucent(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).getIndex();
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {FACING});
+    }
+
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ,
+                                            int meta, EntityLivingBase placer, EnumHand hand) {
+        return this.getDefaultState().withProperty(FACING, facing.getOpposite());
+    }
+
+
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        Block block = worldIn.getBlockState(pos.offset(state.getValue(FACING))).getBlock();
+        if (block instanceof IKineticActor) {
+            List<BlockPos> iteratedBlocks = new ArrayList<>(); //Generate the iteratedBlocks list for using
+            ((IKineticActor) block).passRotation(worldIn, pos.offset(state.getValue(FACING)), state.getValue(FACING).getOpposite(),
+                    iteratedBlocks, false, false);
+        }
+    }
+}
