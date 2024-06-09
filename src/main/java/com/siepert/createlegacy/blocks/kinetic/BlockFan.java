8d27559ae4655d9d24c6bdb6a6ab4e5165528188
Siepert123 <sj.hijmans@gmail.com>
2024-06-09 13:41:26 +0200

    Recipe ?

--- src/main/java/com/siepert/createlegacy/blocks/kinetic/BlockFan.java
+++ src/main/java/com/siepert/createlegacy/blocks/kinetic/BlockFan.java
@@ -5,6 +5,10 @@ import com.siepert.createlegacy.mainRegistry.ModBlocks;
 import com.siepert.createlegacy.mainRegistry.ModItems;
 import com.siepert.createlegacy.util.IHasModel;
 import com.siepert.createlegacy.util.IKineticActor;
+import com.siepert.createlegacy.util.Reference;
+import com.siepert.createlegacy.util.handlers.ModSoundHandler;
+import com.siepert.createlegacy.util.handlers.recipes.PressingRecipes;
+import com.siepert.createlegacy.util.handlers.recipes.WashingRecipes;
 import mcp.MethodsReturnNonnullByDefault;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -15,18 +19,20 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.nio.channels.IllegalSelectorException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -113,6 +119,21 @@ public class BlockFan extends Block implements IHasModel, IKineticActor {
         PROCESSORS.add(Blocks.FLOWING_LAVA);
         PROCESSORS.add(Blocks.FIRE);
     }
+
+    private enum ProcessingType {
+        WASH, SMELT;
+
+        public static @Nullable ProcessingType getType(Block blockIn) {
+            if (blockIn.equals(Blocks.WATER) || blockIn.equals(Blocks.FLOWING_WATER)) {
+                return WASH;
+            }
+            if (blockIn.equals(Blocks.LAVA) || blockIn.equals(Blocks.FLOWING_LAVA)|| blockIn.equals(Blocks.FIRE)) {
+                return SMELT;
+            }
+            return null;
+        }
+    }
+
     @Override
     public void passRotation(World worldIn, BlockPos pos, EnumFacing source, List<BlockPos> iteratedBlocks, boolean srcIsCog, boolean srcCogIsHorizontal) {
         if (srcIsCog) return; //We don't accept a cog as input, we need a shaft!
@@ -125,8 +146,32 @@ public class BlockFan extends Block implements IHasModel, IKineticActor {
 
             boolean isABlower = !PROCESSORS.contains(worldIn.getBlockState(pos.offset(source.getOpposite())).getBlock());
 
+            BlockPos posFront = pos.offset(source.getOpposite());
+
+            double particleVX = source.getOpposite().getFrontOffsetX() * 2;
+            double particleVY = source.getOpposite().getFrontOffsetY() * 2;
+            double particleVZ = source.getOpposite().getFrontOffsetZ() * 2;
+
             if (isABlower) {
-                if (!worldIn.getBlockState(pos.offset(source.getOpposite())).getMaterial().blocksMovement()) {
+                if (!worldIn.getBlockState(posFront).getMaterial().blocksMovement()) {
+                    worldIn.spawnParticle(EnumParticleTypes.CLOUD,
+                            posFront.getX() + 0.5,
+                            posFront.getY() + 0.5,
+                            posFront.getZ() + 0.5,
+                            particleVX,
+                            particleVY,
+                            particleVZ);
+                    for (int i = 0; i < Reference.random.nextInt(4) + 4; i++) {
+                        worldIn.spawnParticle(EnumParticleTypes.CLOUD,
+                                posFront.getX() + Reference.random.nextFloat(),
+                                posFront.getY() + Reference.random.nextFloat(),
+                                posFront.getZ() + Reference.random.nextFloat(),
+                                particleVX,
+                                particleVY,
+                                particleVZ);
+                    }
+
+
                     AxisAlignedBB searchField = new AxisAlignedBB(pos.offset(source.getOpposite()));
 
                     List<Entity> foundEntities = worldIn.getEntitiesWithinAABB(Entity.class, searchField);
@@ -234,6 +279,88 @@ public class BlockFan extends Block implements IHasModel, IKineticActor {
                     }
                 } else return;
             }
+
+            ProcessingType whatsTheProcess = ProcessingType.getType(worldIn.getBlockState(posFront).getBlock());
+
+            if (whatsTheProcess != null) {
+                BlockPos startProcessPos = pos.offset(source.getOpposite(), 2);
+                int processingLength = 0;
+
+                Block blockNow = worldIn.getBlockState(startProcessPos).getBlock();
+
+                while (processingLength < 6 && !blockNow.getMaterial(worldIn.getBlockState(startProcessPos.offset(source.getOpposite(), processingLength))).blocksMovement()) {
+                    processingLength++;
+                    blockNow = worldIn.getBlockState(startProcessPos.offset(source.getOpposite(), processingLength)).getBlock();
+                }
+
+                BlockPos endPos = startProcessPos.offset(source.getOpposite(), processingLength);
+
+                AxisAlignedBB itemSearchArea = new AxisAlignedBB(startProcessPos, endPos);
+                List<EntityItem> foundItems = worldIn.getEntitiesWithinAABB(EntityItem.class, itemSearchArea);
+
+                for (EntityItem entityItem : foundItems) {
+                    if (apply(entityItem.getItem(), whatsTheProcess).hasRecipe) {
+                        EntityItem resultEntityItem = new EntityItem(worldIn, entityItem.posX, entityItem.posY, entityItem.posZ,
+                                apply(entityItem.getItem(), whatsTheProcess).stack);
+                        resultEntityItem.setVelocity(0, 0, 0);
+                        worldIn.spawnEntity(resultEntityItem);
+                        entityItem.getItem().shrink(1);
+                        if (entityItem.getItem().getCount() == 0 || entityItem.getItem().isEmpty()) {
+                            entityItem.setDead();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public @Nullable ResultSet apply(ItemStack stack, ProcessingType type) {
+        if (stack.isEmpty()) {
+            return new ResultSet(stack, false);
+        }
+        else {
+            if (type == ProcessingType.WASH) {
+                ItemStack itemstack = WashingRecipes.instance().getWashingResult(stack);
+
+                if (itemstack.isEmpty()) {
+                    CreateLegacy.logger.warn("Couldn't wash {} because there is no washing recipe", (Object) stack);
+                    return new ResultSet(stack, false);
+                } else {
+                    ItemStack itemstack1 = itemstack.copy();
+                    itemstack1.setCount(itemstack.getCount());
+                    return new ResultSet(itemstack1, true);
+                }
+            }
+            if (type == ProcessingType.SMELT) {
+                ItemStack itemstack = FurnaceRecipes.instance().getSmeltingResult(stack);
+
+                if (itemstack.isEmpty()) {
+                    CreateLegacy.logger.warn("Couldn't wash {} because there is no washing recipe", (Object) stack);
+                    return new ResultSet(stack, false);
+                } else {
+                    ItemStack itemstack1 = itemstack.copy();
+                    itemstack1.setCount(itemstack.getCount());
+                    return new ResultSet(itemstack1, true);
+                }
+            }
+            return null;
+        }
+    }
+
+    private class ResultSet {
+        ItemStack stack;
+        boolean hasRecipe;
+        private ResultSet(ItemStack stack, boolean hasRecipe) {
+            this.stack = stack;
+            this.hasRecipe = hasRecipe;
+        }
+
+        public boolean hasRecipe() {
+            return hasRecipe;
+        }
+
+        public ItemStack getResult() {
+            return stack;
         }
     }
 }
