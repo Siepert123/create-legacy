72a27e8e01495fa66248619f3046771e7821dfef
Siepert123 <sj.hijmans@gmail.com>
2024-06-15 15:36:47 +0200

    blazeburner sfx

--- src/main/java/com/siepert/createlegacy/tileentity/TileEntityBlazeBurner.java
+++ src/main/java/com/siepert/createlegacy/tileentity/TileEntityBlazeBurner.java
@@ -1,10 +1,13 @@
 package com.siepert.createlegacy.tileentity;
 
 import com.siepert.createlegacy.blocks.kinetic.BlockBlazeBurner;
+import com.siepert.createlegacy.util.Reference;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.SoundCategory;
 
 public class TileEntityBlazeBurner extends TileEntity implements ITickable {
     private int remainingBurnTime;
@@ -38,9 +41,41 @@ public class TileEntityBlazeBurner extends TileEntity implements ITickable {
             if (!world.isRemote) {
                 IBlockState myNewState = world.getBlockState(pos).withProperty(BlockBlazeBurner.SCHEDULE, 0);
                 if (remainingBurnTime > 2400) {
+                    if (world.getTotalWorldTime() % 15 == 0) {
+                        if (Reference.random.nextInt(3) == 0) world.playSound(null,
+                                pos.getX() + 0.5,
+                                pos.getY() + 0.5,
+                                pos.getZ() + 0.5,
+                                SoundEvents.ENTITY_BLAZE_BURN,
+                                SoundCategory.BLOCKS,
+                                0.7f, 1.24f);
+                        if (Reference.random.nextInt(7) == 0) world.playSound(null,
+                                pos.getX() + 0.5,
+                                pos.getY() + 0.5,
+                                pos.getZ() + 0.5,
+                                SoundEvents.ENTITY_BLAZE_AMBIENT,
+                                SoundCategory.BLOCKS,
+                                0.3f, 1.0f);
+                    }
                     remainingBurnTime--;
                     BlockBlazeBurner.setState(myNewState.withProperty(BlockBlazeBurner.STATE, BlockBlazeBurner.State.COPE_SEETHE_MALD), world, pos);
                 } else if (remainingBurnTime > 0) {
+                    if (world.getTotalWorldTime() % 35 == 0) {
+                        if (Reference.random.nextInt(3) == 0) world.playSound(null,
+                                pos.getX() + 0.5,
+                                pos.getY() + 0.5,
+                                pos.getZ() + 0.5,
+                                SoundEvents.ENTITY_BLAZE_BURN,
+                                SoundCategory.BLOCKS,
+                                0.7f, 1.24f);
+                        if (Reference.random.nextInt(7) == 0) world.playSound(null,
+                                pos.getX() + 0.5,
+                                pos.getY() + 0.5,
+                                pos.getZ() + 0.5,
+                                SoundEvents.ENTITY_BLAZE_AMBIENT,
+                                SoundCategory.BLOCKS,
+                                0.3f, 1.0f);
+                    }
                     remainingBurnTime--;
                     BlockBlazeBurner.setState(myNewState.withProperty(BlockBlazeBurner.STATE, BlockBlazeBurner.State.HEATED), world, pos);
                 } else {
