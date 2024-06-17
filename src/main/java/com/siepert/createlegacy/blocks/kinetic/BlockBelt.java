c04f4b36df78a6509bf301400303d4b10a2f7728
Siepert123 <sj.hijmans@gmail.com>
2024-06-17 18:28:17 +0200

    Belt!

--- src/main/java/com/siepert/createlegacy/blocks/kinetic/BlockBelt.java
+++ src/main/java/com/siepert/createlegacy/blocks/kinetic/BlockBelt.java
@@ -24,6 +24,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
@@ -31,7 +32,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BlockBelt extends Block implements IHasModel, IKineticActor {
-    public static final PropertyBool HAS_SHAFT = PropertyBool.create("has_shaft");
+    public static final PropertyBool HAS_AXLE = PropertyBool.create("has_axle");
     public static final PropertyEnum<EnumFacing.Axis> AXIS = PropertyEnum.create("axis", EnumFacing.Axis.class);
     public BlockBelt(String name) {
         super(Material.CLOTH, MapColor.BLACK);
@@ -40,12 +41,12 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
         this.fullBlock = false;
         setLightOpacity(0);
 
-        setDefaultState(this.blockState.getBaseState().withProperty(HAS_SHAFT, false).withProperty(AXIS, EnumFacing.Axis.X));
+        setDefaultState(this.blockState.getBaseState().withProperty(HAS_AXLE, false).withProperty(AXIS, EnumFacing.Axis.X));
 
         setUnlocalizedName("create:" + name);
         setRegistryName(name);
         setCreativeTab(CreateLegacy.TAB_CREATE);
-        setHarvestLevel("shears", -1);
+        setHarvestLevel("axe", 0);
         setHardness(1);
         setResistance(2);
         ModBlocks.BLOCKS.add(this);
@@ -60,29 +61,34 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
     @Override
     public int getMetaFromState(IBlockState state) {
         int meta = 0;
-        if (state.getValue(HAS_SHAFT)) meta += 2;
+        if (state.getValue(HAS_AXLE)) meta += 2;
         if (state.getValue(AXIS) == EnumFacing.Axis.Z) meta++;
         return meta;
+    }
+    public static final AxisAlignedBB bb = new AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 14.0 / 16.0, 1.0);
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return bb;
     }
 
     @Override
     public IBlockState getStateFromMeta(int meta) {
         switch (meta) {
             case 0:
-                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.X).withProperty(HAS_SHAFT, false);
+                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.X).withProperty(HAS_AXLE, false);
             case 1:
-                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.Z).withProperty(HAS_SHAFT, false);
+                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.Z).withProperty(HAS_AXLE, false);
             case 2:
-                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.X).withProperty(HAS_SHAFT, true);
+                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.X).withProperty(HAS_AXLE, true);
             case 3:
-                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.Z).withProperty(HAS_SHAFT, true);
+                return this.getDefaultState().withProperty(AXIS, EnumFacing.Axis.Z).withProperty(HAS_AXLE, true);
         }
         return this.getStateFromMeta(0);
     }
 
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] {HAS_SHAFT, AXIS});
+        return new BlockStateContainer(this, new IProperty[] {HAS_AXLE, AXIS});
     }
 
     @Override
@@ -105,7 +111,7 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
 
         if (source.getAxis() == myState.getValue(AXIS)) return;
 
-        if (myState.getValue(HAS_SHAFT)) {
+        if (myState.getValue(HAS_AXLE)) {
             iteratedBlocks.add(pos);
         } else return;
 
@@ -129,9 +135,13 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
         }
 
         List<BlockPos> iteratedBelts = new ArrayList<>();
-
-        commenceItemMovement(worldIn, pos.offset(movement), movement.getOpposite(), iteratedBelts, movement);
-        commenceItemMovement(worldIn, pos.offset(movement.getOpposite()), movement, iteratedBelts, movement);
+        iteratedBelts.add(pos);
+        if (worldIn.getBlockState(pos.offset(movement)).getBlock() instanceof BlockBelt) {
+            commenceItemMovement(worldIn, pos.offset(movement), movement.getOpposite(), iteratedBelts, movement, iteratedBlocks, inverseRotation);
+        }
+        if (worldIn.getBlockState(pos.offset(movement.getOpposite())).getBlock() instanceof BlockBelt) {
+            commenceItemMovement(worldIn, pos.offset(movement.getOpposite()), movement, iteratedBelts, movement, iteratedBlocks, inverseRotation);
+        }
 
         if (worldIn.getBlockState(pos.offset(source.getOpposite())).getBlock() instanceof IKineticActor) {
             ((IKineticActor) worldIn.getBlockState(pos.offset(source.getOpposite())).getBlock())
@@ -139,8 +149,10 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
         }
     }
 
-    public void commenceItemMovement(World worldIn, BlockPos pos, EnumFacing source, List<BlockPos> iteratedBelts, EnumFacing movement) {
+    public void commenceItemMovement(World worldIn, BlockPos pos, EnumFacing source, List<BlockPos> iteratedBelts, EnumFacing movement, List<BlockPos> iteratedBlocks, boolean inv) {
         iteratedBelts.add(pos);
+
+        IBlockState myState = worldIn.getBlockState(pos);
 
         if (!worldIn.isRemote) {
             AxisAlignedBB bb = new AxisAlignedBB(pos.up());
@@ -158,17 +170,43 @@ public class BlockBelt extends Block implements IHasModel, IKineticActor {
                 && !iteratedBelts.contains(pos.offset(source.getOpposite()))) {
             if (worldIn.getBlockState(pos.offset(source.getOpposite())).getValue(AXIS) == worldIn.getBlockState(pos).getValue(AXIS)) {
                 ((BlockBelt) worldIn.getBlockState(pos.offset(source.getOpposite())).getBlock())
-                        .commenceItemMovement(worldIn, pos.offset(source.getOpposite()), source, iteratedBelts, movement);
+                        .commenceItemMovement(worldIn, pos.offset(source.getOpposite()), source, iteratedBelts, movement, iteratedBlocks, inv);
             }
         }
+
+        if (worldIn.getBlockState(pos).getBlock() instanceof BlockBelt) {
+            Block blockAtP = worldIn.getBlockState(pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), true))).getBlock();
+            Block blockAtN = worldIn.getBlockState(pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), false))).getBlock();
+
+            if (blockAtP instanceof IKineticActor && !iteratedBlocks.contains(pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), true)))) {
+                ((IKineticActor) blockAtP).passRotation(worldIn, pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), true)),
+                        fromAx(myState.getValue(AXIS), false), iteratedBlocks,
+                        false, false, inv);
+            }
+            if (blockAtN instanceof IKineticActor && !iteratedBlocks.contains(pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), false)))) {
+                ((IKineticActor) blockAtN).passRotation(worldIn, pos.offset(fromAx(rotateAxis(myState.getValue(AXIS)), false)),
+                        fromAx(rotateAxis(myState.getValue(AXIS)), true), iteratedBlocks,
+                        false, false, inv);
+            }
+        } else CreateLegacy.logger.error("Why am I not a belt?? {}", pos);
     }
 
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (playerIn.getHeldItem(hand).getItem() == Item.getItemFromBlock(ModBlocks.AXLE) && !state.getValue(HAS_SHAFT)) {
-            worldIn.setBlockState(pos, state.withProperty(HAS_SHAFT, true));
+        if (playerIn.getHeldItem(hand).getItem() == Item.getItemFromBlock(ModBlocks.AXLE) && !state.getValue(HAS_AXLE)) {
+            worldIn.setBlockState(pos, state.withProperty(HAS_AXLE, true));
             return true;
         }
         return false;
+    }
+
+    public EnumFacing.Axis rotateAxis(EnumFacing.Axis axis) {
+        if (axis == EnumFacing.Axis.X) return EnumFacing.Axis.Z;
+        return EnumFacing.Axis.X;
+    }
+
+    public EnumFacing fromAx(EnumFacing.Axis axis, boolean b) {
+        if (b) return EnumFacing.getFacingFromAxis(EnumFacing.AxisDirection.POSITIVE, axis);
+        return EnumFacing.getFacingFromAxis(EnumFacing.AxisDirection.NEGATIVE, axis);
     }
 }
