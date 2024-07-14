2d7a0b4690917143d605c6a985a7bcd47f744b5a
Siepert123 <sj.hijmans@gmail.com>
2024-07-14 19:03:00 +0200

    Small test thing

--- src/main/java/com/siepert/createapi/CreateAPI.java
+++ src/main/java/com/siepert/createapi/CreateAPI.java
@@ -1,6 +1,9 @@
 package com.siepert.createapi;
 
 import com.siepert.createlegacy.util.Reference;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class CreateAPI {
     public static boolean compareCreateVersions(int versionOther) {
@@ -13,5 +16,29 @@ public class CreateAPI {
 
     public static int getKineticVersion() {
         return Reference.KINETIC_VERSION;
+    }
+
+    public static int discoverRotation(World world, BlockPos pos, EnumFacing.Axis axis) {
+        switch (axis) {
+            case X:
+                if (pos.getY() % 2 == pos.getZ() % 2) {
+                    return (int) world.getTotalWorldTime() % 4 + 1;
+                } else {
+                    return (int) world.getTotalWorldTime() % 4;
+                }
+            case Y:
+                if (pos.getX() % 2 == pos.getZ() % 2) {
+                    return (int) world.getTotalWorldTime() % 4 + 1;
+                } else {
+                    return (int) world.getTotalWorldTime() % 4;
+                }
+            case Z:
+                if (pos.getY() % 2 == pos.getX() % 2) {
+                    return (int) world.getTotalWorldTime() % 4 + 1;
+                } else {
+                    return (int) world.getTotalWorldTime() % 4;
+                }
+        }
+        return 0;
     }
 }
