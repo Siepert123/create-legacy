ab88d3b9a1ccc7df2c692f583beb4c3387cc8fc3
Siepert123 <sj.hijmans@gmail.com>
2024-07-19 21:24:28 +0200

    new version

--- src/main/java/com/siepert/createlegacy/CreateLegacyModData.java
+++ src/main/java/com/siepert/createlegacy/CreateLegacyModData.java
@@ -13,8 +13,8 @@ import java.util.Random;
 public final class CreateLegacyModData {
     public static final String MOD_ID = "create"; //Mod ID; will never change
     public static final String NAME = "Create Legacy"; //Mod name; will never change
-    public static final String VERSION = "24w29d"; //The version, based on snapshot naming scheme
-    public static final int VERSION_NUMBER = 5;
+    public static final String VERSION = "24w29e"; //The version, based on snapshot naming scheme
+    public static final int VERSION_NUMBER = 6;
     public static final int KINETIC_VERSION = 2;
     public static final String CLIENT_PROXY = "com.siepert.createlegacy.proxy.ClientProxy"; //Client proxy class
     public static final String COMMON_PROXY = "com.siepert.createlegacy.proxy.CommonProxy"; //Common proxy class
