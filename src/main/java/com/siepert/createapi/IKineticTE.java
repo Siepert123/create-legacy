dd696eb566417b4dac22ad389dab88f5b7faeafb
Siepert123 <sj.hijmans@gmail.com>
2024-08-12 16:16:36 +0200

    JavaDoc

--- src/main/java/com/siepert/createapi/IKineticTE.java
+++ src/main/java/com/siepert/createapi/IKineticTE.java
@@ -4,12 +4,15 @@ import net.minecraft.util.EnumFacing;
 
 /**
  * The IKineticTE is an interface for TileEntities.
- * If you want to add a kinetic block (eg. a centrifuge or something idk) it is REQUIRED that this interface is implemented.
+ * If you want to add a kinetic block (e.g. a centrifuge or something IDK) it is REQUIRED that this interface is implemented.
  *
  * @author Siepert123
  * */
 public interface IKineticTE {
 
+    /**
+     * @return The stress impact. Final SU consumption is calculated by multiplying this number with the network speed.
+     */
     double getStressImpact();
 
     default int getMinimalSpeed() {
@@ -20,6 +23,9 @@ public interface IKineticTE {
         return getStressImpact() != 0;
     }
 
+    /**
+     * @return The stress capacity. Final SU production is calculated by multiplying this number with the produced speed.
+     */
     double getStressCapacity();
 
     int getProducedSpeed();
@@ -28,6 +34,9 @@ public interface IKineticTE {
         return getStressCapacity() != 0;
     }
 
+    /**
+     * @return True if the machine should still be activated when the network is overstressed.
+     */
     default boolean ignoreOverstress() {
         return false;
     }
@@ -36,5 +45,16 @@ public interface IKineticTE {
 
     void setUpdated();
 
+    /**
+     * Similar to passRotation, except the kinetic actions are not performed.
+     * Only add a kinetic block instance if it is actually going to be activated.
+     *
+     * @param context NetworkContext. Main use is to check if a block is already added to the network,
+     *                and to add this block instance to the network.
+     * @param source The direction the signal came from.
+     * @param srcIsCog Whether the input is from a cogwheel.
+     * @param srcCogIsHorizontal If the cogwheel was horizontal.
+     * @param inverted If the rotation rotates the other way.
+     */
     void passNetwork(NetworkContext context, EnumFacing source, boolean srcIsCog, boolean srcCogIsHorizontal, boolean inverted);
 }
