efe7b6a9ad9d9fff7731a148fa450f5180a33fcd
Siepert123 <createlegacy69@gmail.com>
2024-09-15 15:40:20 +0200

    simplistic tupular class

---
+++ src/main/java/com/melonstudios/createlegacy/util/SimpleTuple.java
@@ -0,0 +1,27 @@
+package com.melonstudios.createlegacy.util;
+
+public class SimpleTuple<X, Y> {
+    public SimpleTuple(X value1, Y value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+
+    public void setValue1(X value1) {
+        this.value1 = value1;
+    }
+    private X value1;
+    public X getValue1() {
+        return value1;
+    }
+    public void setValue2(Y value2) {
+        this.value2 = value2;
+    }
+    private Y value2;
+    public Y getValue2() {
+        return value2;
+    }
+
+    public SimpleTuple<X, Y> copy() {
+        return new SimpleTuple<>(value1, value2);
+    }
+}
