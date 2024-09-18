ddf4e2a0c490cc968b6f3afb1d83da128777fcc1
Siepert123 <createlegacy69@gmail.com>
2024-09-18 21:43:15 +0200

    version set to 0.1.0

--- src/main/java/com/melonstudios/createlegacy/CreateLegacy.java
+++ src/main/java/com/melonstudios/createlegacy/CreateLegacy.java
@@ -19,7 +19,7 @@ import static com.melonstudios.createlegacy.CreateLegacy.VERSION;
 @Mod(modid = MOD_ID, name = "Create Legacy", version = VERSION)
 public final class CreateLegacy {
     public static final String MOD_ID = "create";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.1.0";
 
     public static final int VERSION_NUM = 0;
     public static final int KINETIC_VERSION_NUM = 0;
