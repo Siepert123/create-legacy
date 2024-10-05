0a49bac4878a9ec25fa853e482d8d40b963817a3
Siepert123 <createlegacy69@gmail.com>
2024-10-05 11:50:15 +0200

    hello is this amon gus

---
+++ create-legacy-mod/src/main/java/com/melonstudios/createlegacy/mixin/MixinEntityMinecart.java
@@ -0,0 +1,18 @@
+package com.melonstudios.createlegacy.mixin;
+
+import net.minecraft.entity.item.EntityMinecart;
+import net.minecraft.util.math.AxisAlignedBB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityMinecart.class)
+public abstract class MixinEntityMinecart {
+
+    @Inject(method = "getMaximumSpeed", at = @At("HEAD"), cancellable = true)
+    public void injectGetMaximumSpeed(CallbackInfoReturnable<Float> cir) {
+        // Set the return value to 0.0 to change the maximum speed of the minecart
+        cir.setReturnValue(0.0f);
+    }
+}
