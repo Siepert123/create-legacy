f396c625182f5f305b09efed2bb9b496fd9808c8
Siepert123 <createlegacy69@gmail.com>
2024-11-01 17:59:33 +0100

    aaaaa

--- create-legacy-mod/src/main/java/com/melonstudios/createlegacy/contraption/ContraptionBearing.java
+++ create-legacy-mod/src/main/java/com/melonstudios/createlegacy/contraption/ContraptionBearing.java
@@ -1,5 +1,7 @@
 package com.melonstudios.createlegacy.contraption;
 
+import com.melonstudios.createlegacy.tileentity.abstractions.AbstractTileEntityBearing;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 
 import java.util.ArrayList;
@@ -15,4 +17,16 @@ public class ContraptionBearing {
     }
 
     List<TileEntity> tileEntities = new ArrayList<>();
+
+    IBlockState[][][] structure;
+
+    int sizeX, sizeY, sizeZ;
+    AbstractTileEntityBearing te;
+
+    public ContraptionBearing(int sizeX, int sizeY, int sizeZ, AbstractTileEntityBearing te) {
+        this.sizeX  = sizeX;
+        this.sizeY  = sizeY;
+        this.sizeZ  = sizeZ;
+        this.te     = te;
+    }
 }
