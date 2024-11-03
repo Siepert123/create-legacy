081b0c1a895febfe9bfae681c8ec8a0c51a7d3e4
Siepert123 <createlegacy69@gmail.com>
2024-11-03 10:30:44 +0100

    fluid???

--- create-legacy-mod/src/main/java/com/melonstudios/createlegacy/fluid/FluidChocolate.java
+++ create-legacy-mod/src/main/java/com/melonstudios/createlegacy/fluid/FluidChocolate.java
@@ -1,19 +1,18 @@
 package com.melonstudios.createlegacy.fluid;
 
-import net.minecraft.item.EnumDyeColor;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class FluidChocolate extends Fluid {
     public static final int color = 8606770;
     public FluidChocolate() {
-        super("chocolate", new ResourceLocation("create", "textures/fluid/chocolate"),
-                new ResourceLocation("create", "textures/fluid/chocolate_flowing"),
+        super("chocolate", new ResourceLocation("create", "fluid/chocolate_still"),
+                new ResourceLocation("create", "fluid/chocolate_flow"),
                 color);
 
         setViscosity(500);
         setDensity(2000);
 
-        setUnlocalizedName("fluid.create.chocolate");
+        setUnlocalizedName("create.chocolate");
     }
 }
