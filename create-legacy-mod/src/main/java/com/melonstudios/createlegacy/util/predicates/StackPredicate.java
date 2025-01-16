4ea22dae88a90dc6f14e201306b17768d5a195ac
Siepert123 <createlegacy69@gmail.com>
2025-01-16 18:21:18 +0100

    Removed files that are now in MelonLib

--- create-legacy-mod/src/main/java/com/melonstudios/createlegacy/util/predicates/StackPredicate.java
+++
@@ -1,7 +0,0 @@
-package com.melonstudios.createlegacy.util.predicates;
-
-import net.minecraft.item.ItemStack;
-
-public abstract class StackPredicate {
-    public abstract boolean matches(ItemStack stack);
-}
